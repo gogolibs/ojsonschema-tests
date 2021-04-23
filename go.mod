e25afc9a178b3834532c6f8181690f1c303a1856
Kurkin <deemson.k@gmail.com>
2021-04-23 13:27:40 +0300

    added test cases with real jsonschema validator

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/gogolibs/ojsonschema-tests
+
+go 1.16
+
+require (
+	github.com/gogolibs/ojson v0.0.0-20210423100730-fbedf322930c
+	github.com/gogolibs/ojsonschema v0.0.0-20210423101119-e238d8e4fd47
+	github.com/qri-io/jsonschema v0.2.1
+	github.com/stretchr/testify v1.7.0
+)
---
+++ object_test.go
@@ -0,0 +1,92 @@
+package ojsonschema_tests
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gogolibs/ojson"
+	"github.com/gogolibs/ojsonschema"
+	"github.com/qri-io/jsonschema"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type validationCase struct {
+	name     string
+	expected []jsonschema.KeyError
+	actual   ojson.Anything
+}
+
+var schemaCases = []struct {
+	name            string
+	schema          ojson.Anything
+	validationCases []validationCase
+}{
+	{
+		name:   "string: simple",
+		schema: ojsonschema.String{},
+		validationCases: []validationCase{
+			{
+				name:     "just a string, no errors",
+				actual:   "hello",
+				expected: []jsonschema.KeyError{},
+			},
+			{
+				name:   "integer instead of string",
+				actual: 42,
+				expected: []jsonschema.KeyError{
+					{PropertyPath: "/", InvalidValue: 42, Message: "type should be string, got integer"},
+				},
+			},
+		},
+	},
+	{
+		name: "object: single required field, no additional properties",
+		schema: ojsonschema.Object{
+			AdditionalProperties: false,
+			Properties: ojson.Object{
+				"field": ojsonschema.String{},
+			},
+			Required: ojson.Array{"field"},
+		},
+		validationCases: []validationCase{
+			{
+				name:     "valid case",
+				actual:   ojson.Object{"field": "hello"},
+				expected: []jsonschema.KeyError{},
+			},
+			{
+				name:   "missing required field and unknown field is present",
+				actual: ojson.Object{"unknown-field": "hello"},
+				expected: []jsonschema.KeyError{
+					{
+						PropertyPath: "/",
+						InvalidValue: map[string]interface{}{"unknown-field": "hello"},
+						Message:      `"field" value is required`,
+					},
+					{
+						PropertyPath: "/",
+						InvalidValue: map[string]interface{}{"unknown-field": "hello"},
+						Message:      "additional properties are not allowed",
+					},
+				},
+			},
+		},
+	},
+}
+
+func TestSchemaCases(t *testing.T) {
+	for _, schemaCase := range schemaCases {
+		t.Run(schemaCase.name, func(t *testing.T) {
+			schemaData := ojson.MustMarshal(schemaCase.schema)
+			schema := new(jsonschema.Schema)
+			err := json.Unmarshal(schemaData, schema)
+			require.NoError(t, err)
+			for _, validationCase := range schemaCase.validationCases {
+				t.Run(validationCase.name, func(t *testing.T) {
+					state := schema.Validate(context.Background(), validationCase.actual)
+					require.Equal(t, validationCase.expected, *state.Errs)
+				})
+			}
+		})
+	}
+}
